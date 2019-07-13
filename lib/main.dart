cee4eb6c86c684baa8a2a547d7eb4aafd3fd0349
Htue Ko <htooko1101@gmail.com>
2019-07-14 01:27:02 +0630

    add images

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,25 @@ import 'package:first_easy_list/ui/appBar.dart';
 import 'package:flutter/material.dart';
 
 
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   String appbarTitle = "This is awesome";
-
-  Color appbarBackgroundColour = Colors.blueAccent;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: ReusableAppBar.getAppBar(appbarTitle),
+        body: Card(
+          child: Column(
+            children: <Widget>[
+              Image.asset("assets/images/one.jpg"),
+              Text("Awesome List"),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/ui/appBar.dart
+++ lib/ui/appBar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 // appbar widget
 class ReusableAppBar {
+
   static getAppBar(String title){
     return AppBar(
       title: Text(title),
